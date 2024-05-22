40d14d60556aafb10900518b55643584cd1a7555
Ivan <ivanlucas.dev@gmail.com>
2024-05-22 19:40:55 -0300

    refactor

--- src/api/config.ts
+++ src/api/config.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: process.env.URL_API,
   headers: {
-    Accept: 'application/json',
     'Content-Type': 'application/json',
   },
 })
